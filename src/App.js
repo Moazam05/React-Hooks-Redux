4393ca88ae36943909ccaa25d9b98206e5959c8b
Moazam05 <salmanmoazam08@gmail.com>
2020-08-24 10:49:09 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import './App.css';
+
+import Home from './Components/ChNoThree/home';
+
+function App() {
+  return (
+    <div className='App'>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ChNoThree/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class home extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hello</h3>
+      </div>
+    );
+  }
+}
+
+export default home;

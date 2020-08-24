7b6f320a6a2268b019c4b6f1b54878e982ee51cf
Moazam05 <salmanmoazam08@gmail.com>
2020-08-24 15:10:39 +0500

    Getting a User Physical Location

--- src/Components/ChNoFour/season.js
+++ src/Components/ChNoFour/season.js
@@ -3,6 +3,10 @@ import React from 'react';
 import SeasonDisplay from './SeasonDisplay';
 
 const Season = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (err) => console.log(err)
+  );
   return (
     <>
       <h3>Hi There!</h3>

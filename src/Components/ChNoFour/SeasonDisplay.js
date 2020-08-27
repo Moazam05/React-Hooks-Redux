d718a137f0f0d8d4b5dfcae4a433070b55c0ab11
Moazam05 <salmanmoazam08@gmail.com>
2020-08-27 11:06:24 +0500

    Extracting Options to Config Objects

--- src/Components/ChNoFour/SeasonDisplay.js
+++ src/Components/ChNoFour/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const seasonConfig = {
+  Summer: {
+    text: "Let's hit the beach",
+    iconName: 'sun',
+  },
+  Winter: {
+    text: 'Burr it is cold!',
+    iconName: 'snowflake',
+  },
+};
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? 'Summer' : 'Winter';
@@ -11,10 +22,20 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
 
+  const { text, iconName } = seasonConfig[season];
+  // const text =
+  //   season === 'Winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
+
+  // const icon = season === 'Winter' ? 'snowflake' : 'sun';
+
   console.log(season);
   return (
     <>
-      <h3>Season Display</h3>
+      <i className={`${iconName} icon`} />
+      <h1>{text}</h1>
+      {/* <i className={icon} /> */}
+      <i className={`${iconName} icon`} />
+      {/* <i className='disabled users icon'></i> */}
     </>
   );
 };

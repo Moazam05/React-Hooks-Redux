276840b89f2fbc83b02ec04caf6921628f80dcaa
Moazam05 <salmanmoazam08@gmail.com>
2020-08-28 15:25:43 +0500

    Axios setup for Fetching data from Unsplash

--- src/Components/ChNoSeven/Car.js
+++ src/Components/ChNoSeven/Car.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
+import axios from 'axios';
+
 import SearchBar from './SearchBar';
 
 export class Car extends Component {
   onSearchSubmit(term) {
-    console.log(term);
+    // console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID DWoxu0TqVdgTSHg2R0cyeY6Ew958b-_0kblxGKMB2S0',
+      },
+    });
   }
   render() {
     return (

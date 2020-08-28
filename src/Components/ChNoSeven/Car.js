b8c3a76efee847e538a20c36fe8c202e791b6df2
Moazam05 <salmanmoazam08@gmail.com>
2020-08-28 15:56:41 +0500

    Binding Callbacks

--- src/Components/ChNoSeven/Car.js
+++ src/Components/ChNoSeven/Car.js
@@ -5,20 +5,24 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 export class Car extends Component {
-  async onSearchSubmit(term) {
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
         Authorization: 'Client-ID DWoxu0TqVdgTSHg2R0cyeY6Ew958b-_0kblxGKMB2S0',
       },
     });
-    console.log(response.data.results);
-  }
+    console.log(this);
+    this.setState({ images: response.data.results });
+  };
   render() {
     return (
       <>
         <div className='ui container' style={{ marginTop: '10px' }}>
           <SearchBar onSubmit={this.onSearchSubmit} />
+          Found: {this.state.images.length} images
         </div>
       </>
     );

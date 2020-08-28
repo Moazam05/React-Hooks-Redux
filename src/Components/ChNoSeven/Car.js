c76a39b9e2e9a84aa8794760039efda9da38430a
Moazam05 <salmanmoazam08@gmail.com>
2020-08-28 15:35:49 +0500

    Handling Requests with Async Await

--- src/Components/ChNoSeven/Car.js
+++ src/Components/ChNoSeven/Car.js
@@ -5,14 +5,14 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 export class Car extends Component {
-  onSearchSubmit(term) {
-    // console.log(term);
-    axios.get('https://api.unsplash.com/search/photos', {
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
         Authorization: 'Client-ID DWoxu0TqVdgTSHg2R0cyeY6Ew958b-_0kblxGKMB2S0',
       },
     });
+    console.log(response.data.results);
   }
   render() {
     return (

0af009c2071ec88eb52345b3d85e35bd99d94cf4
Moazam05 <salmanmoazam08@gmail.com>
2020-08-31 11:02:25 +0500

    Callbacks on Image Load

---
+++ src/Components/ChNoSeven/ImageCard.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+export class ImageCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    console.log(this.imageRef.current.clientHeight);
+  };
+
+  render() {
+    const { description, urls } = this.props.image;
+
+    return (
+      <>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
+      </>
+    );
+  }
+}
+
+export default ImageCard;

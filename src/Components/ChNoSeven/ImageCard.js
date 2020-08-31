69e6f3c4dd97ba3c493855e9970d5081c10ac08a
Moazam05 <salmanmoazam08@gmail.com>
2020-08-31 11:19:27 +0500

    Dynamic Spans

--- src/Components/ChNoSeven/ImageCard.js
+++ src/Components/ChNoSeven/ImageCard.js
@@ -4,6 +4,8 @@ export class ImageCard extends Component {
   constructor(props) {
     super(props);
 
+    this.state = { spans: 0 };
+
     this.imageRef = React.createRef();
   }
 
@@ -12,7 +14,11 @@ export class ImageCard extends Component {
   }
 
   setSpans = () => {
-    console.log(this.imageRef.current.clientHeight);
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans });
   };
 
   render() {
@@ -20,7 +26,9 @@ export class ImageCard extends Component {
 
     return (
       <>
-        <img ref={this.imageRef} alt={description} src={urls.regular} />
+        <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+          <img ref={this.imageRef} alt={description} src={urls.regular} />
+        </div>
       </>
     );
   }

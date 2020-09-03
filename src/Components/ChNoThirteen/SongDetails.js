0987d3657da8a38ac81b87a31f43ddd7cf61081d
Moazam05 <salmanmoazam08@gmail.com>
2020-09-03 10:49:39 +0500

    Conditional Rendering

--- src/Components/ChNoThirteen/SongDetails.js
+++ src/Components/ChNoThirteen/SongDetails.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SongDetails = (props) => {
-  console.log(props);
-  return <div>Song Details</div>;
+const SongDetails = ({ song }) => {
+  if (!song) {
+    return <div>Select a Song</div>;
+  }
+  return (
+    <div>
+      <h3>Details for:</h3>
+      <p>
+        Title: {song.title}
+        <br />
+        Duration: {song.duration}
+      </p>
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
